9ed3e57fb3be39b8b2638f461f72db441a8bb46f
Iván Guerrero <ivangvprogrammer@gmail.com>
2023-12-01 14:44:57 -0500

    [ADD] exercise 3.4: backend of the phone book, step 4: delete a single entry from the phone book

--- index.js
+++ index.js
@@ -53,6 +53,14 @@ app.get('/api/persons/:id', (request, response) => {
     }
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+    console.log(id);
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server runing on port ${PORT}`);
